54227d25b2600372d2d73f3b9c082e15d35c4a12
migue <miguelrmsilva@gmail.com>
2025-01-19 15:30:27 +0000

    ele agarrou-se kinda

--- miguel/scripts/simpler_monkey.gd
+++ miguel/scripts/simpler_monkey.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 const JUMP_VELOCITY = -400.0
 const ARM_MAX_DISTANCE = 50.0
@@ -6,44 +6,63 @@ const ARM_MAX_DISTANCE = 50.0
 const LEFT_ARM_OFFSET = Vector2(-32, 0)
 const RIGHT_ARM_OFFSET = Vector2(32, 0)
 
-@onready var left_hand = $LeftHand
-@onready var right_hand = $RightHand
+@onready var left_hand = get_node("../LeftHand")
+@onready var right_hand = get_node("../RightHand")
 
 var left_arm_axis = Vector2.ZERO
 var right_arm_axis = Vector2.ZERO
 
+var left_hand_can_grab = null
+var right_hand_can_grab = null
+
+var is_left_grabbing = false
+
 func _process(delta: float) -> void:
 	# Get joystick input
-	var left_x = Input.get_axis("left_left", "left_right")
-	var left_y = Input.get_axis("left_up", "left_down")
+	var left_x = Input.get_axis("move_leftarm_left", "move_leftarm_right")
+	var left_y = Input.get_axis("move_leftarm_up", "move_leftarm_down")
 	
-	var right_x = Input.get_axis("right_left", "right_right")
-	var right_y = Input.get_axis("right_up", "right_down")
+	var right_x = Input.get_axis("move_rightarm_left", "move_rightarm_right")
+	var right_y = Input.get_axis("move_rightarm_up", "move_rightarm_down")
 	
 	left_arm_axis = Vector2(left_x, left_y) * ARM_MAX_DISTANCE
 	
 	right_arm_axis = Vector2(right_x, right_y) * ARM_MAX_DISTANCE
-
+	
+	if Input.is_action_pressed("grab_left") and left_hand_can_grab != null and is_left_grabbing == false: #need to save which object it was
+		print("idk")
+		
+		#var pin_joint = PinJoint2D.new()
+		#pin_joint.global_position = left_hand.global_position # Set joint's position to body_a's position
+		#pin_joint.node_a = left_hand.get_path() # Path to the first body
+		#pin_joint.node_b = left_hand_can_grab.get_path() # Path to the second body
+		#add_child(pin_joint)
+		is_left_grabbing = true
 	# Print the result for debugging
-	print("Left Arm Axis:", left_arm_axis)
-	print("Right Arm Axis:", right_arm_axis)
+	#print("Left Arm Axis:", left_arm_axis)
+	#print("Right Arm Axis:", right_arm_axis)
 
 func _physics_process(delta: float) -> void:
 	# Gravity
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
 
 	# Left Hand
-	var left_hand_offset = left_arm_axis
-	left_hand.global_position = self.global_position + left_hand_offset + LEFT_ARM_OFFSET
+	if !is_left_grabbing:
+		var left_hand_offset = left_arm_axis
+		left_hand.global_position = self.global_position + left_hand_offset + LEFT_ARM_OFFSET
+	else:
+		self.global_position = left_hand.global_position
 
 	# Right Hand
 	var right_hand_offset = right_arm_axis
 	right_hand.global_position = self.global_position + right_hand_offset + RIGHT_ARM_OFFSET
 
-	# Character movement
-	move_and_slide()
+
+func _on_lefthand_body_entered(body: Node2D) -> void:
+	left_hand_can_grab = body
+	print("LeftHand can grab")
+
+func _on_lefthand_body_exited(body: Node2D) -> void:
+	left_hand_can_grab = false
+	print("LeftHand cant grab")
